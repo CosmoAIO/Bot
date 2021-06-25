b1a7a8303c98fa24bd8cff1c7b8c623a550846c4
DreezySZN <unboundkicks@gmail.com>
2021-06-26 00:02:59 +0100

    Added function to load tasks.

--- tasks.py
+++ tasks.py
@@ -1,27 +1,41 @@
 import csv
+import json
 
 
 class TaskManager():
+    def __init__(self, file):
+        self.file = file
+
+    # either in 'loadTasks' or 'returnTasks' add a list which stores two distionary "task-info" & "profile-info". append each task-info and profile-info for each task created eg:
+    # task: {task1, task-info:, profile-info}, {task2. task-info:, profile-info:} etc....
 
     def loadTasks(self):
-        with open("tasks.csv") as tasks:
+        print("Loading tasks.")
+
+        self.tasks = []
+        with open(self.file) as tasks:
             tasks_csv = csv.DictReader(tasks)
             for task in tasks_csv:
-                with open("profiles.csv") as profiles:
-                    profiles_csv = csv.DictReader(profiles)
-                    for profile in profiles_csv:
+                with open("profiles.json") as profiles:
+                    profiles_json = json.load(profiles)
+                    for profile in profiles_json['profiles']:
                         try:
-                            if task['PROFILE'] == profile['PROFILE_NAME']:
+                            if task['PROFILE'] == profile['profileName']:
                                 self.profile = profile
                                 self.task = task
+                                self.tasks.append(task)
                                 print(task)
-                                continue
                             else:
-                                break
+                                continue
                         except Exception as e:
                             print(e)
-                            continue
-                    print(self.profile['FIRSTNAME'])
+
+        i = len(self.tasks)
+        print(f"Loaded {i} tasks.")
+
+    def returnTasks(self):
+        pass
 
 
-TaskManager().loadTasks()
+task = TaskManager("tasks.csv")  # can use ("{foldername}/tasks.csv") to get a directory location
+task.loadTasks()
